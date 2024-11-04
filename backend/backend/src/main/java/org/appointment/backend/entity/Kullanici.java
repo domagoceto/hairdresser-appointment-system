1928818314bf3bca362b5fa83525f83c92f79cab
Hatice Zehra Türel <90724716+zehraturel@users.noreply.github.com>
2024-11-04 17:01:56 +0300

    veritabanı bağlandı

--- backend/backend/src/main/java/org/appointment/backend/entity/Kullanici.java
+++ backend/backend/src/main/java/org/appointment/backend/entity/Kullanici.java
@@ -1,10 +1,9 @@
 package org.appointment.backend.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+
+
 import lombok.Getter;
 import lombok.Setter;
 
