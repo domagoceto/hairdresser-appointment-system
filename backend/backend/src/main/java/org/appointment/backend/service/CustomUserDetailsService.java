6d2439f8de22538806e8ff005f40762a9bc33eff
Kadir Çetin <bjkkadir121@gmail.com>
2024-11-19 21:10:45 +0300

    bok

--- backend/backend/src/main/java/org/appointment/backend/service/CustomUserDetailsService.java
+++
@@ -1,8 +0,0 @@
-package org.appointment.backend.service;
-
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-public interface CustomUserDetailsService {
-    UserDetails loadUserByUsername(String email) throws UsernameNotFoundException;
-}
